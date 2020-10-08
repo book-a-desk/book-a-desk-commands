092d3ffaebaf8b02a319a49329c4f4089f6d2342
Bogdan Dragan <37081655+bogdangd@users.noreply.github.com>
2020-10-08 08:54:35 +0200

    Created web application

--- Book-A-Desk.Api/Program.fs
+++ Book-A-Desk.Api/Program.fs
@@ -1,8 +1,28 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
 
-open System
+let webApp =
+    choose [
+        route "/hello"   >=> text "Hello, world!" ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
