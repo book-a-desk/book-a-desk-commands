b4d34378249f4306d7d666d65a142ceda0d63de0
Stefan Senff <stefan.senff@broadsign.com>
2020-11-05 18:27:18 +0100

    2nd workshop day - Extending implementation  (#7)
    * Creation of Command Handler.
    
    * Fixed file to let code compile
    
    * Fix typo
    
    * Implement DeskBooked event type
    
    * Return the DeskBooked event from the command handler
    
    * Store events from command handler
    
    * Fix formatting
    
    * Add simple InMemoryEventStore
    
    * Return a Result from the storeEvent function
    
    * Made handleBookaDesk handler
    
    * Refactoring code using discriminated unions and provide instances of functions
    
    * Open API specification.
    
    * API Specification changes.
    
    1. Created "docs" folder and moved API specification json into it.
    2. Changed 405 status (coming from the Swagger template), to 400, for invalid input.
    
    * Extend .gitignore and fixed summary in OpenAPI specification
    
    * deleted unnecessary files
    
    Co-authored-by: noemi-hernandez <60353586+noemi-hernandez@users.noreply.github.com>
    Co-authored-by: Tomas Morton <tomas.morton@broadsign.com>
    Co-authored-by: Cpt0Teemo <ypoulmarck@gmail.com>

--- Book-A-Desk.Api/HttpHandlers.fs
+++ Book-A-Desk.Api/HttpHandlers.fs
@@ -1,6 +1,18 @@
 ﻿module BookADesk.Api.HttpHandlers
 
 open Giraffe
+open Microsoft.AspNetCore.Http
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Bookadesk.Commands.Domain
 
-let handleBookADesk : HttpHandler =
-    Successful.OK "Ok"
+let handleBookADesk (next : HttpFunc) (ctx : HttpContext) =
+    task {
+        let! model = ctx.BindModelAsync<BookADesk>()
+        let command = BookADesk model
+        let commandHandler = BookADeskCommandHandler.provide InMemoryEventStore.storeEvent
+
+        let result = commandHandler.Handle command
+        match result with
+        | Ok _ -> return! json model next ctx
+        | Error e -> return! failwith e
+    }
