c90473c75e412da538ffa4071673f466ad6d924f
Hriday Sur <hridaysur@yahoo.co.in>
2020-12-16 11:55:02 -0500

    [offices] map offices from domain

--- Book-A-Desk.Api/OfficesHttpHandler.fs
+++ Book-A-Desk.Api/OfficesHttpHandler.fs
@@ -1,8 +1,10 @@
 namespace Book_A_Desk.Api
 
-
 open Giraffe
 open FSharp.Control.Tasks.V2.ContextInsensitive
+
+open Book_A_Desk.Api.Models
+open Book_A_Desk.Domain.Office.Domain
 
 type OfficesHttpHandler =
     {
@@ -13,7 +15,15 @@ module OfficesHttpHandler =
     let initialize =
         let handleGet () = fun next context ->
             task {
-                let offices = [||]
+                let offices =
+                    Offices.All
+                    |> List.map (fun o ->
+                        let (OfficeId officeId) = o.Id
+                        let (CityName cityName) = o.City
+                        {
+                            Id = officeId.ToString()
+                            Name = cityName
+                        })
                 return! json offices next context
             }
         {
