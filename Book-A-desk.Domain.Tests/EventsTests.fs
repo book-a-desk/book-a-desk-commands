4e9c523035fe7ba0d4cef78c830fcac2e8ae2115
AlexGagne <alex.gagne@broadsign.com>
2021-02-15 17:37:48 -0500

    fixing tests + adding more

--- Book-A-desk.Domain.Tests/EventsTests.fs
+++ Book-A-desk.Domain.Tests/EventsTests.fs
@@ -9,14 +9,26 @@ open Book_A_Desk.Domain.Reservation.Events
 open Xunit
 
 [<Fact>]
-let ``GIVEN an aggregateId and events with an invalid aggregateId, when reading them, reading fails`` () =
+let ``GIVEN an aggregateId and events with an invalid aggregateId and map not empty, when reading them, reading fails`` () =
+    let aPreviousAggregateId = Guid.NewGuid ()
     let aggregateId = Guid.NewGuid ()
-    let events = Map.empty
+    let events = (aPreviousAggregateId, List.empty, Map.empty) |||> Map.add
     
     let result = EventsReader.getEvents aggregateId events
     match result with
     | Ok _ -> failwith "should have failed"
     | Error _ -> ()
+    
+[<Fact>]
+let ``GIVEN an aggregateId and events with an empty map, when reading them, returns an empty list`` () =
+    let aggregateId = Guid.NewGuid ()
+    let events = Map.empty
+    
+    let result = EventsReader.getEvents aggregateId events
+    match result with
+    | Ok [] -> ()
+    | Error _ -> failwith "should not have failed"
+    | Ok _ -> failwith "should not return something else"
     
 [<Fact>]
 let ``GIVEN an aggregateId and events with a valid aggregateId, when reading them, reading succeeds`` () =
@@ -81,3 +93,33 @@ let ``GIVEN previous and new events, when appending, events are appended`` () =
     
     Assert.True(List.contains eventForAggregate eventsResult)
     
+[<Fact>]
+let ``GIVEN previous and new events, when appending, previous events are not overwritten`` () =
+    let aggregateId = Guid.NewGuid ()    
+    let previousEvent =
+        {
+            ReservationId = Guid.NewGuid () |> ReservationId
+            Date = DateTime.MaxValue
+            EmailAddress = EmailAddress "anEmail"
+            OfficeId = Guid.NewGuid () |> OfficeId
+        }
+        |> DeskBooked
+        |> ReservationEvent
+    
+    let previousEvents =  (aggregateId, [previousEvent], Map.empty) |||> Map.add
+    
+    let eventForAggregate =
+        {
+            ReservationId = Guid.NewGuid () |> ReservationId
+            Date = DateTime.MaxValue
+            EmailAddress = EmailAddress "emailAddress"
+            OfficeId = Guid.NewGuid () |> OfficeId
+        }
+        |> DeskBooked
+        |> ReservationEvent
+    let newEvents = (aggregateId, [eventForAggregate], Map.empty) |||> Map.add
+    
+    let result = EventsWriter.appendEvents previousEvents newEvents
+    let eventsResult = result.[aggregateId]
+    
+    Assert.True(List.contains previousEvent eventsResult)
