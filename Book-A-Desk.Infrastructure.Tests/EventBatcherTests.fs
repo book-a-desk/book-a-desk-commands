d64a5a8322cb2155f496a2de0e233940a6fc0ceb
ARASH-PC\Arash.Arasteh <arash@ayudasystems.com>
2021-03-08 12:24:04 -0500

    no message

--- Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
+++ Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
@@ -1,8 +1,14 @@
 module Tests
 
 open System
+open Book_A_Desk.Infrastructure
 open Xunit
 
 [<Fact>]
-let ``Empty`` () =
-    Assert.True(true)
+let ``Given An EventBatcher When Batching An Empty List Returns Empty List`` () =
+    let result = EventBatcher.batchEvents Map.empty 
+    Assert.Equal(0, Seq.length result)
+    
+[<Fact>]
+let ``Given An EventBatcher When Batching Less Than 25 Events Returns A Single Batch``() =
+    let batch =  seq { yield events }
