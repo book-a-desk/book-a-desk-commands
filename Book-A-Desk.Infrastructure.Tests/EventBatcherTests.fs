7f5ac5ea39312374e6d9e311ded5f311f4587d59
noemi-hernandez <60353586+noemi-hernandez@users.noreply.github.com>
2021-03-08 13:03:31 -0500

    Noemi

--- Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
+++ Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
@@ -23,7 +23,9 @@ let ``Given An EventBatcher When Batching Less Than 25 Events Returns A Single B
     let batch = Map.empty.Add(Guid.NewGuid(), generateDeskBooked () |> Seq.take 1)
     let result = EventBatcher.batchEvents batch
     
-    Assert.Single(result)
+    let resultBatch = Assert.Single(result)
+    Assert.Single(resultBatch)
+    
 
 [<Fact>]
 let ``Given An EventBatcher When Batching More Than 25 Events Returns Multiple Batches`` () =
@@ -39,5 +41,11 @@ let ``Given An EventBatcher When Batching More Than 25 Events Returns Multiple B
 let ``Given an EventBatcher When Batching Multiple Aggregates Returns Multiple Batches`` () =
     let desksBooked1 = generateDeskBooked () |> Seq.take 5
     let desksBooked2 = generateDeskBooked () |> Seq.take 7
-    let batch = Map.empty.Add(Guid.NewGuid())
-    
+    let batch = Map.empty
+                   .Add(Guid.NewGuid(), desksBooked1)
+                   .Add(Guid.NewGuid(), desksBooked2)
+    let result = EventBatcher.batchEvents batch
+
+    Assert.Equal(2, Seq.length result)
+    Assert.Equal(7, Seq.length(Seq.item 0 result))
+    Assert.Equal(5, Seq.length(Seq.item 1 result))
