efd644a9302a1659a388db2953c4d6dcddedd0c9
Tomas Morton <tomas.morton@broadsign.com>
2021-03-08 12:52:46 -0500

    WIP new test for multiple aggregates

--- Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
+++ Book-A-Desk.Infrastructure.Tests/EventBatcherTests.fs
@@ -3,25 +3,6 @@ module Tests
 open System
 open Book_A_Desk.Infrastructure
 open Xunit
-
-[<Fact>]
-let ``Given An EventBatcher When Batching An Empty List Returns Empty List`` () =
-    let result = EventBatcher.batchEvents Map.empty 
-    Assert.Equal(0, Seq.length result)
-    
-[<Fact>]
-let ``Given An EventBatcher When Batching Less Than 25 Events Returns A Single Batch``() =
-    let deskBooked =
-        {
-            AggregateId = Guid.NewGuid()
-            Date = DateTime.MaxValue
-            EmailAddress = "test@test.com"
-            OfficeId = Guid.NewGuid()
-        }
-    let batch = Map.empty.Add(Guid.NewGuid(), Seq.singleton deskBooked)
-    let result = EventBatcher.batchEvents batch
-    
-    Assert.Single(result)
     
 let generateDeskBooked () =
     Seq.initInfinite (fun i ->
@@ -33,6 +14,18 @@ let generateDeskBooked () =
         })
     
 [<Fact>]
+let ``Given An EventBatcher When Batching An Empty List Returns Empty List`` () =
+    let result = EventBatcher.batchEvents Map.empty 
+    Assert.Equal(0, Seq.length result)
+    
+[<Fact>]
+let ``Given An EventBatcher When Batching Less Than 25 Events Returns A Single Batch``() =
+    let batch = Map.empty.Add(Guid.NewGuid(), generateDeskBooked () |> Seq.take 1)
+    let result = EventBatcher.batchEvents batch
+    
+    Assert.Single(result)
+
+[<Fact>]
 let ``Given An EventBatcher When Batching More Than 25 Events Returns Multiple Batches`` () =
     let desksBooked = generateDeskBooked () |> Seq.take 30
     let batch = Map.empty.Add(Guid.NewGuid(), desksBooked)
@@ -41,3 +34,10 @@ let ``Given An EventBatcher When Batching More Than 25 Events Returns Multiple B
     Assert.Equal(2, Seq.length result)
     Assert.Equal(25, Seq.length(Seq.item 0 result))
     Assert.Equal(5, Seq.length(Seq.item 1 result))
+    
+[<Fact>]
+let ``Given an EventBatcher When Batching Multiple Aggregates Returns Multiple Batches`` () =
+    let desksBooked1 = generateDeskBooked () |> Seq.take 5
+    let desksBooked2 = generateDeskBooked () |> Seq.take 7
+    let batch = Map.empty.Add(Guid.NewGuid())
+    
