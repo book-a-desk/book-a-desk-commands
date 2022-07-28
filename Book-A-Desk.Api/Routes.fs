936f4ac3b8613997828a6f290f946ceb142f4639
AlexGagne <alex_gagne94@hotmail.com>
2022-07-28 09:33:08 -0400

    Okta integration (#71)
    * initial steps
    
    * oktaIntegration: added doc example
    
    * Adding Okta token verification
    
    * Small fixes
    
    * oktaingration: add environment variables to docker compose file
    
    * deployment test for okta
    
    * only deploy to dev
    
    * Keeping both variables.
    
    * oktaintegration: fix circleci config
    
    Co-authored-by: AlexGagne <alex.gagne@broadsign.com>
    Co-authored-by: Mario Gonzalez <87493499+mariogonzalezbs@users.noreply.github.com>
    Co-authored-by: jimmyc <jimmy.cyr@broadsign.com>
    Co-authored-by: noemi-hernandez <60353586+noemi-hernandez@users.noreply.github.com>

--- Book-A-Desk.Api/Routes.fs
+++ Book-A-Desk.Api/Routes.fs
@@ -1,6 +1,9 @@
 ﻿namespace Book_A_Desk.Api
 
+open FSharp.Control.Tasks.V2.ContextInsensitive
 open Giraffe
+open Microsoft.AspNetCore.Http
+open System.Threading.Tasks
 
 open Book_A_Desk.Api.Models
 
@@ -8,22 +11,59 @@ type Routes =
     {
         HttpHandlers: HttpHandler
     }
+    
+type TokenValidationResult =
+| ValidToken
+| InvalidToken of string
+    
 module Routes =
-    let provide (apiDependencyFactory:ApiDependencyFactory) =
+    let private failAuthorization message context =
+        let failHandler = (text message >=> setStatusCode 401)
+        failHandler earlyReturn context
+    
+    let private authorize
+        (validateToken : string -> Task<TokenValidationResult>)
+        : HttpHandler
+        = fun next (context : HttpContext) -> task {
+        
+        let bearerToken =
+            context.GetRequestHeader("Authorization")
+        
+        match bearerToken with
+        | Ok bearerToken ->            
+            let bearerToken = bearerToken.Replace("Bearer ", "")
+            
+            match! validateToken bearerToken with
+            | ValidToken ->
+                return! next context
+            | InvalidToken message ->
+                return! failAuthorization message context
+        | Error e ->
+            return! failAuthorization $"Could not get bearer token: {e}" context
+    }
+
+    let provide (apiDependencyFactory:ApiDependencyFactory) validateToken =
         let httpHandlers : HttpHandler =
             choose [
                 GET >=> choose [
-                    route "/offices" >=> (
+                    route "/offices" >=>
+                    (authorize validateToken) >=>
+                    (
                         apiDependencyFactory.CreateOfficesHttpHandler ()
                         |> fun h -> h.HandleGetAll ()
                     )
                     routef "/offices/%O/availabilities" (
-                        apiDependencyFactory.CreateOfficesHttpHandler ()
-                        |> fun h -> h.HandleGetByDate
-                    )
+                        fun officeId ->
+                            (authorize validateToken) >=>
+                            (
+                                apiDependencyFactory.CreateOfficesHttpHandler ()
+                                |> fun h -> (h.HandleGetByDate officeId)
+                            ))
                 ]
                 POST >=> choose [
-                    route "/bookings" >=> JsonBodyValidator.parseBody<Booking> (
+                    route "/bookings" >=>
+                    (authorize validateToken) >=>
+                    JsonBodyValidator.parseBody<Booking> (
                         apiDependencyFactory.CreateBookingsHttpHandler ()
                         |> fun h -> h.HandlePostWith
                     )
