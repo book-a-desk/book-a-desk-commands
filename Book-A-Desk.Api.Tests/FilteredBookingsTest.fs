cf585c994ef2e6450d5e343982c14c1c637ab7da
Lasnik <lasnikprogram@gmail.com>
2023-07-26 15:06:33 +0200

    improve tidiness

--- Book-A-Desk.Api.Tests/FilteredBookingsTest.fs
+++ Book-A-Desk.Api.Tests/FilteredBookingsTest.fs
@@ -9,36 +9,33 @@ open Book_A_Desk.Domain.Office.Domain
 open Book_A_Desk.Infrastructure.DynamoDbEventStore
 open Book_A_Desk.Domain
 open Book_A_Desk.Infrastructure
-
 open Book_A_Desk.Domain.Events
 open Book_A_Desk.Domain.Reservation
 open Book_A_Desk.Domain.Reservation.Queries
-
 open Book_A_Desk.Domain.Reservation.Events
-
 
 let officeId = Guid.NewGuid ()
 let date = DateTime(2100,02,01)
 
-let createDeskBookedEvent date officeId email :DomainEvent =
+let createDeskBookedEvent date officeId email : DomainEvent =
     ({
             Date = date
-            EmailAddress = "anEmail" |> EmailAddress
+            EmailAddress = "lasnikr" |> EmailAddress
             OfficeId = officeId |> OfficeId
     }: Events.DeskBooked) |> DeskBooked |> ReservationEvent
-let bookings: DomainEvent seq = 
+
+let bookings : DomainEvent seq = 
     [
         createDeskBookedEvent date officeId "lasnikr"
         createDeskBookedEvent date officeId "user"
         createDeskBookedEvent date officeId "dummy"
     ]
-        
 
 let mockProvideEventStore =
-        {
-            GetEvents = fun _ -> bookings |> Ok |> async.Return
-            AppendEvents = fun _ -> failwith "should not be called"
-        } : DynamoDbEventStore.DynamoDbEventStore
+    {
+        GetEvents = fun _ -> bookings |> Ok |> async.Return
+        AppendEvents = fun _ -> failwith "should not be called"
+    } : DynamoDbEventStore.DynamoDbEventStore
 
 let getBookingEvents : Option<seq<DomainEvent>> =
     async {
@@ -49,32 +46,26 @@ let getBookingEvents : Option<seq<DomainEvent>> =
     }
     |> Async.RunSynchronously
 
-let getBookingsFrom (bookingEvents: Option<seq<DomainEvent>>) (date : DateTime option)  (officeId : OfficeId option) (email : EmailAddress option) =
+let getBookingsFrom (bookingEvents : Option<seq<DomainEvent>>) (date : DateTime option)  (officeId : OfficeId option) (email : EmailAddress option) =
     match bookingEvents with 
     | Some bookingEvents -> 
         let result = ReservationsQueriesHandler.getFilteredBookings bookingEvents date officeId email
         match result with
         | Ok bookings ->
-            let bookings =
-                bookings
-                |> List.map (fun (booking:Booking) ->
-                    Booking.value booking.OfficeId booking.Date booking.EmailAddress)
-                |> List.toArray
-                |> fun l -> { Bookings.Items = l }
-            bookings
-        | Error e -> failwith"Error occured"
-    | None -> failwith"Empty booking events"
+           bookings
+
+        | Error e -> failwith "Error occured"
+    | None -> failwith "Empty booking events"
 
 [<Fact>]
 let ``GIVEN booking events and a date WHEN filtering bookings THEN all bookings for the date are returned`` () = async {
-
     let bookingEvents = getBookingEvents
     let bookings = getBookingsFrom bookingEvents (Some date) None None
     
-    Assert.Equal(bookings.Items.Length, 3)
-    Assert.Equal(date, bookings.Items[0].Date)
-    Assert.Equal(date, bookings.Items[1].Date)
-    Assert.Equal(date, bookings.Items[2].Date)
+    Assert.Equal(bookings.Length, 3)
+    Assert.Equal(date, bookings[0].Date)
+    Assert.Equal(date, bookings[1].Date)
+    Assert.Equal(date, bookings[2].Date)
 }
 
 // [<Fact>]
